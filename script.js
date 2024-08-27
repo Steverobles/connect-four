81818ae95e884683ac42d0101227bf7bd129bda5
Steve Robles <steve@icon-media.com>
2024-08-27 12:11:41 -0700

    completing logic and game

--- script.js
+++ script.js
@@ -1,84 +1,132 @@
+
 /*----- constants -----*/
 const COLORS = {
-    '0': 'White',
-    '1' : 'Red',
-    '-1': 'Blue'
-}
-
-/*----- state variables -----*/
-let board // array of column arrays 
-let turn // 1 or -1
-let winner // null = no winner; 1 or -1 winner; 'T' = tie game
-
-/*----- cached elements  -----*/
-const messageEl = document.querySelector('h1')
-const playAgainBtn = document.querySelector('button')
-const markerEls = document.querySelectorAll('#markers > div')
-
-/*----- event listeners -----*/
-
-
-/*----- functions -----*/
-init();
-
-//Initialize all state, then call render()
-function init() {
-    // To visualize the board's mapping to the DOM,
-    //rotate the board array 90 degrees counter-clockwise
+    '0': 'white',
+    '1': 'blue',
+    '-1': 'red',
+  };
+  
+  /*----- state variables -----*/
+  let board;  // array of 7 column arrays
+  let turn;  // 1 or -1
+  let winner;  // null = no winner; 1 or -1 = winner; 'T' = Tie
+  
+  /*----- cached elements  -----*/
+  const messageEl = document.querySelector('h1');
+  const playAgainBtn = document.querySelector('button');
+  const markerEls = [...document.querySelectorAll('#markers > div')];
+  
+  /*----- event listeners -----*/
+  document.getElementById('markers').addEventListener('click', handleDrop);
+  playAgainBtn.addEventListener('click', init);
+  
+  /*----- functions -----*/
+  init();
+  
+  function init() {
     board = [
-        [0, 0, 0, 0, 0, 0], // col 0
-        [0, 0, 0, 0, 0, 0], // col 1
-        [0, 0, 0, 0, 0, 0], // col 2
-        [0, 0, 0, 0, 0, 0], // col 3
-        [0, 0, 0, 0, 0, 0], // col 4
-        [0, 0, 0, 0, 0, 0], // col 5
-        [0, 0, 0, 0, 0, 0], // col 6
-]
-turn = 1
-winner = null
-render()
-}
+      [0, 0, 0, 0, 0, 0],  // col 0
+      [0, 0, 0, 0, 0, 0],  // col 1
+      [0, 0, 0, 0, 0, 0],  // col 2
+      [0, 0, 0, 0, 0, 0],  // col 3
+      [0, 0, 0, 0, 0, 0],  // col 4
+      [0, 0, 0, 0, 0, 0],  // col 5
+      [0, 0, 0, 0, 0, 0],  // col 6
+    ];
+    turn = 1;
+    winner = null;
+    render();
+  }
 
-// visualize all state in the DOM
-function render() {
-    renderBoard()
-    renderMessage()
-    //hide/show UI elements (controls)
-    renderControls()
-}
+  function handleDrop(evt) {
+    const colIdx = markerEls.indexOf(evt.target);
+    if (colIdx === -1) return;
+    const colArr = board[colIdx];
+    const rowIdx = colArr.indexOf(0);
+    colArr[rowIdx] = turn;
+    turn *= -1;
+    winner = getWinner(colIdx, rowIdx);
+    render();
+  }
+  
 
-function renderBoard() {
-    board.forEach(function(colArr, colIdx) {
-        // iterate over the cells in the current column(colArr)
-        colArr.forEach(function(cellVal, rowIdx) {
-            const cellId = `c${colIdx}r${rowIdx}`
-            const cellEl = document.getElementById(cellId)
-            cellEl.style.backgroundColor = COLORS[cellVal]
-        })
-    })
-}
-
-function renderMessage() {
-    if(winner === 'T') {
-        messageEl.innerText = "It's a tie!"        
-    } else if (winner) {
-        messageEl.innerHTML = `<span style="color: ${COLORS[winner]}">${COLORS[winner]}</span> Wins! `
-    } else {
-        //Game is in play
-        messageEl.innerHTML = `<span style="color: ${COLORS[turn]}">${COLORS[turn]}'s</span> Turn`
+  function getWinner(colIdx, rowIdx) {
+    return checkVerticalWin(colIdx, rowIdx) ||
+      checkHorizontalWin(colIdx, rowIdx) ||
+      checkDiagonalWinNESW(colIdx, rowIdx) ||
+      checkDiagonalWinNWSE(colIdx, rowIdx);
+  }
+  
+  function checkDiagonalWinNWSE(colIdx, rowIdx) {
+    const adjCountNW = countAdjacent(colIdx, rowIdx, -1, 1);
+    const adjCountSE = countAdjacent(colIdx, rowIdx, 1, -1);
+    return (adjCountNW + adjCountSE) >= 3 ? board[colIdx][rowIdx] : null;
+  }
+  
+  function checkDiagonalWinNESW(colIdx, rowIdx) {
+    const adjCountNE = countAdjacent(colIdx, rowIdx, 1, 1);
+    const adjCountSW = countAdjacent(colIdx, rowIdx, -1, -1);
+    return (adjCountNE + adjCountSW) >= 3 ? board[colIdx][rowIdx] : null;
+  }
+  
+  function checkHorizontalWin(colIdx, rowIdx) {
+    const adjCountLeft = countAdjacent(colIdx, rowIdx, -1, 0);
+    const adjCountRight = countAdjacent(colIdx, rowIdx, 1, 0);
+    return (adjCountLeft + adjCountRight) >= 3 ? board[colIdx][rowIdx] : null;
+  }
+  
+  function checkVerticalWin(colIdx, rowIdx) {
+    return countAdjacent(colIdx, rowIdx, 0, -1) === 3 ? board[colIdx][rowIdx] : null;
+  }
+  
+  function countAdjacent(colIdx, rowIdx, colOffset, rowOffset) {
+    const player = board[colIdx][rowIdx];
+    let count = 0;
+    colIdx += colOffset;
+    rowIdx += rowOffset;
+    while (
+      board[colIdx] !== undefined &&
+      board[colIdx][rowIdx] !== undefined &&
+      board[colIdx][rowIdx] === player
+    ) {
+      count++;
+      colIdx += colOffset;
+      rowIdx += rowOffset;
     }
-}
-
-function renderControls () {
-// Ternary expression is the go to when you want 1 of 2 returned
-//< cond expression> ? <truthy exp> : <falsey exp>
-playAgainBtn.style.visibility = winner ? 'visible' : 'hidden'
-// itereate over the marker elements to hide/show 
-//according to the column being full (no 0's) or not
-markerEls.forEach(function (markerEl, colIdx) {
-    const hideMarker = !board[colIdx].includes(0) || winner
-    markerEl.style.visibility = hideMarker ? 'hidden' : 'visible'
-})
-
-
-}
+    return count;
+  }
+  
+  
+  function render() {
+    renderBoard();
+    renderMessage();
+    renderControls();
+  }
+  
+  function renderBoard() {
+    board.forEach(function(colArr, colIdx) {
+      colArr.forEach(function(cellVal, rowIdx) {
+        const cellId = `c${colIdx}r${rowIdx}`;
+        const cellEl = document.getElementById(cellId);
+        cellEl.style.backgroundColor = COLORS[cellVal];
+      });
+    });
+  }
+  
+  function renderMessage() {
+    if (winner === 'T') {
+      messageEl.innerText = "It's a Tie!!!";
+    } else if (winner) {
+      messageEl.innerHTML = `<span style="color: ${COLORS[winner]}">${COLORS[winner].toUpperCase()}</span> Wins!`;
+    } else {
+      messageEl.innerHTML = `<span style="color: ${COLORS[turn]}">${COLORS[turn].toUpperCase()}</span>'s Turn`;
+    }
+  }
+  
+  function renderControls() {
+    playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
+    markerEls.forEach(function(markerEl, colIdx) {
+      const hideMarker = !board[colIdx].includes(0) || winner;
+      markerEl.style.visibility = hideMarker ? 'hidden' : 'visible';
+    });
+  }
